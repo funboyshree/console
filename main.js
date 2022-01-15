8f6b3a83b01f7dee46c6f0651ed6b97bfc94b938
funboyshree <85497935+funboyshree@users.noreply.github.com>
2022-01-16 00:26:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var first_name = "Shrish"
+var last_name = " Sooraj"
+
+concat(first_name & last_name)
+var can = concat()
+console.log(concat)
+var number = "5"
+var number1 = "10"
+
+var add = number + number1
+console.log(add)
